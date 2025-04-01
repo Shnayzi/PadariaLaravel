ffda01004f8dc6b73835853b9e262b285229a82d
Shnayzi <bmtinana@gmail.com>
2025-04-01 19:54:48 -0300

    0.2

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     <title>Principal</title>
 </head>
+
 <body>
-    <h1>Principal</h1>
+    <div class="container0">
+        <h1>Principal</h1>
 
-    <div class="container">
-        <p>Escreva o que você quiser</p>
-        <textarea name="a" id="a" cols="30" rows="10"></textarea>
+        <div>
+            <p>Escreva o que você quiser</p>
+            <textarea name="a" id="a" cols="30" rows="10"></textarea>
+        </div>
+
+        <a href="contato" class="link">Contato</a>
+        <a href="sobre-nos" class="link">Sobre Nós</a>
     </div>
-    
-    <a href="contato" class="link">Contato</a>
-    <a href="sobre-nos" class="link">Sobre Nós</a>
 
+    <div class="container1">
+        <div>
+            <p>Escreva o que você quiser</p>
+            <textarea name="a" id="a" cols="30" rows="10"></textarea>
+        </div>
+
+        <a href="contato" class="link">Contato</a>
+        <a href="sobre-nos" class="link">Sobre Nós</a>
+    </div>
+    </div>
 </body>
+
 </html>
